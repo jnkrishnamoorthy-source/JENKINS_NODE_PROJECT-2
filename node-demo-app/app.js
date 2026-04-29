843faf7dbcfc3135bc2823ef899eebd5a48a9ec8
jnkrishnamoorthy-source <jnkrishnamoorthy@gmail.com>
2026-04-29 15:21:00 +0530

    Updated app-2

--- node-demo-app/app.js
+++ node-demo-app/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Students-3');
+  res.send('Hello Students-4');
 });
 
 app.listen(3000, () => {
